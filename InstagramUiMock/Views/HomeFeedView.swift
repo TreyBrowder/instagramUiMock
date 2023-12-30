b5122cf7b0df24bc6c94d4a31602be617f44cdae
Treybrowder <tbrowder7@gmail.com>
2023-12-30 17:24:30 -0500

    Added all files for the views, add bottom nav bar, added privacy needs to access photo library and camera

---
+++ InstagramUiMock/Views/HomeFeedView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeFeedView.swift
+//  InstagramUiMock
+//
+//  Created by Trey Browder on 12/30/23.
+//
+
+import SwiftUI
+
+struct HomeFeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeFeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeFeedView()
+    }
+}
