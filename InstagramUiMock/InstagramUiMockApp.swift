5ee4ad515b2283c0c618f2eaff9c1822efce2a6b
Treybrowder <tbrowder7@gmail.com>
2023-12-30 15:52:10 -0500

    empty starter project

---
+++ InstagramUiMock/InstagramUiMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUiMockApp.swift
+//  InstagramUiMock
+//
+//  Created by Trey Browder on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUiMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
