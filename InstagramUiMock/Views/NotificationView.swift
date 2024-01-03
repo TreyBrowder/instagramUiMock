0cd5f928b6aa17fe91f05c665bbf475e65d3a91d
Treybrowder <tbrowder7@gmail.com>
2024-01-02 19:49:57 -0500

    added finished notification view - planned layout for profile view

--- InstagramUiMock/Views/NotificationView.swift
+++ InstagramUiMock/Views/NotificationView.swift
@@ -9,12 +9,101 @@ import SwiftUI
 
 struct NotificationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack{
+                List(0...70, id: \.self) { num in
+                    if(num % 2 == 0) {
+                        NotificationFollowView()
+                    }else {
+                        NotificationLikeView()
+                    }
+                }
+            }
+            .navigationTitle("Notifications")
+        }
     }
 }
+
+
+struct NotificationLikeView: View {
+    var body: some View {
+        HStack {
+            //user image
+            let userImageName = "person\(Int.random(in: 1...8))"
+            Image("person\(Int.random(in: 1...8))")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 30, height: 30, alignment: .center)
+                .cornerRadius(20)
+            
+            //Text date
+            VStack(alignment: .leading){
+                Text("Tim Cook Liked your post. ")
+                Text("\(Int.random(in: 2...59)) mins ago ")
+                    .foregroundColor(Color(.secondaryLabel))
+                
+            }
+            
+            Spacer()
+            
+            //Post
+            //generate a random number corresponding to the post number
+            let imageName = "post\(Int.random(in: 1...8))"
+            let image = Image(imageName)
+            
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 30, height: 30, alignment: .trailing)
+            
+            NavigationLink(
+                destination: PostView(userImageName: userImageName, imageName: imageName),
+                label: {
+                    Spacer()
+                    image
+                    
+                }
+            )
+            .frame(width: 30, height: 30, alignment: .center)
+        }
+    }
+}
+    
+    struct NotificationFollowView: View {
+        var body: some View {
+            HStack {
+                //user image
+                Image("person\(Int.random(in: 1...8))")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .cornerRadius(20)
+                
+                //Text date
+                VStack(alignment: .leading){
+                    Text("John started following you. ")
+                    Text("\(Int.random(in: 2...59)) mins ago ")
+                        .foregroundColor(Color(.secondaryLabel))
+                }
+                
+                Spacer()
+                //follow button
+                Button {
+                    //do nothing only focusing on the Ui
+                } label: {
+                    Text("Follow")
+                        .frame(width: 80, height: 30, alignment: .center)
+                        .foregroundColor(.white)
+                        .background(Color(.link))
+                        .cornerRadius(8)
+                }
+                
+            }
+        }
+    }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
         NotificationView()
+            .preferredColorScheme(.dark)
     }
 }
--- InstagramUiMock/Views/ProfileView.swift
+++ InstagramUiMock/Views/ProfileView.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                //HStack: profile pic with number of post/followers/following
+                
+                //Vstack: name and bio
+                
+                //HStack: edit profile and share profile
+                
+                //HStack - Scroll View: Highlight Stories
+                
+                //gridview: post
+                
+            }
+            .navigationTitle("userName Here")
+        }
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+            .preferredColorScheme(.dark)
     }
 }
